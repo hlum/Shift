00fab0e9a814d4414210f67755c978f513c4815b
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-05-02 10:27:17 +0900

    created ShiftUseCase

---
+++ Shift/UseCases/ShiftUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  ShiftUseCase.swift
+//  Shift
+//
+//  Created by cmStudent on 2025/05/02.
+//
+
+import Foundation
+import SwiftData
+
+class ShiftUseCase {
+    private let shiftRepository: ShiftRepository
+    
+    init(shiftRepository: ShiftRepository) {
+        self.shiftRepository = shiftRepository
+    }
+    
+    
+    
+    func fetchShifts(descriptor: FetchDescriptor<Shift>) throws -> [Shift] {
+        return try shiftRepository.fetchShifts(descriptor: descriptor)
+    }
+    
+    
+    func addShift(_ shift: Shift) throws {
+        try shiftRepository.addShift(shift)
+    }
+    
+    
+    func updateShift(_ shift: Shift) throws {
+        try shiftRepository.updateShift(shift)
+    }
+    
+    
+    func deleteShift(_ shift: Shift) throws {
+        try shiftRepository.deleteShift(shift)
+    }
+}
