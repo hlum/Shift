fc9c8d3975e71aa46ddfb136af726795cf846457
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-05-14 12:09:46 +0900

    create HolidayUseCase

---
+++ Shift/UseCases/HolidayUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  HolidayUseCase.swift
+//  Shift
+//
+//  Created by cmStudent on 2025/05/14.
+//
+
+import Foundation
+
+final class HolidayUseCase {
+    private let holidayRepository: HolidayRepository
+    
+    init(holidayRepository: HolidayRepository) {
+        self.holidayRepository = holidayRepository
+    }
+    
+    func fetchHolidays(countryCode: String) async throws -> [Holiday] {
+        return try await holidayRepository.fetchHolidays(countryCode: countryCode)
+    }
+    
+    func fetchHoliday(for date: Date, countryCode: String) async throws -> [Holiday] {
+        return try await holidayRepository.fetchHolidays(for: date, countryCode: countryCode)
+    }
+}
