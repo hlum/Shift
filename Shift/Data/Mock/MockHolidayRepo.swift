ea30ef5cd8d6c1ff5f5e878fea1a3366858eb9d0
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-05-14 18:01:44 +0900

    create MockHolidayRepo

---
+++ Shift/Data/Mock/MockHolidayRepo.swift
@@ -0,0 +1,25 @@
+//
+//  MockHolidayRepo.swift
+//  Shift
+//
+//  Created by cmStudent on 2025/05/14.
+//
+
+import Foundation
+
+final class MockHolidayRepo: HolidayRepository {
+    let holidays: [Holiday] = [
+        Holiday(name: "こどもの日", date: Calendar.current.date(byAdding: .day,value: 1, to: Date())!, countryCode: "JP"),
+        Holiday(name: "〜の日", date: Calendar.current.date(byAdding: .day,value: 3, to: Date())!, countryCode: "JP")
+
+    ]
+    func fetchHolidays(for date: Date, countryCode: String) async throws -> [Holiday] {
+        return holidays
+    }
+    
+    func fetchHolidays(countryCode: String) async throws -> [Holiday] {
+        return holidays
+    }
+    
+    
+}
